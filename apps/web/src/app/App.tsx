97e8ad87a91097aad33c675967d3bdb0e7ecdd4f
vini <vinicius.yonezawa@gmail.com>
2026-07-09 10:56:59 -0300

    .

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -2,7 +2,6 @@ import { AlertTriangle, Sparkles, Telescope } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 import { api } from "../api/client";
-import alexandriaIcon from "../assets/alexandria-icon.png";
 import { useGraphEngine } from "../hooks/useGraphEngine";
 import { detectGalaxies } from "../model/galaxies";
 import { buildGraph } from "../model/graph";
